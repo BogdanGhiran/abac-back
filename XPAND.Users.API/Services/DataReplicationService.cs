259f4aac7ebb08fe292367cccf45bd90aa433010
Bogdan Ghiran <bogdanghiran@gmail.com>
2020-05-04 13:31:43 +0300

    extract planet microservice link into config

--- XPAND.Users.API/Services/DataReplicationService.cs
+++ XPAND.Users.API/Services/DataReplicationService.cs
@@ -2,15 +2,23 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Common.Helpers;
+using Microsoft.Extensions.Configuration;
 using XPAND.Captains.API.Models;
 
 namespace XPAND.Captains.API.Services
 {
     public class DataReplicationService: IDataReplicationService
     {
+        private readonly IConfiguration _config;
+
+        public DataReplicationService(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public async Task RegisterCaptainInPlanetMicroservice(Captain captain, Shuttle shuttle)
         {
-            var uri = new Uri("http://localhost:56521/crewMetaData/");
+            var uri = new Uri(_config["PlanetAPIUrl"] +"crewMetaData/");
             await HttpHelper.PostAsync(uri, new
             {
                 CaptainIdentifier = captain.Identifier,
